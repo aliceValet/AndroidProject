fcfb92848c044353c0ba3989a59e58539c5153c6
Alice VALET <alice_valet@orange.fr>
2019-04-02 01:42:56 +0200

    added edit text to add a new game and marker related to event. Still error to catch though if address not found

--- app/src/main/java/threadlifecycle/moi/android/fr/project/Localisation.java
+++ app/src/main/java/threadlifecycle/moi/android/fr/project/Localisation.java
@@ -3,7 +3,9 @@ package threadlifecycle.moi.android.fr.project;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -18,7 +20,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -28,21 +33,27 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 
 import static java.util.Objects.requireNonNull;
 
 public class Localisation extends Fragment implements LocationListener, OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener {
-    TextView latitudeField;
-    TextView longitudeField;
+        GoogleMap.OnMyLocationClickListener{
+    EditText GameAddress;
+    Button Add;
     LocationManager locationManager;
     String provider;
+    String direccion;
     Location location;
     MapView test;
 
     Marker userLocation;
+
+    Geocoder address;
 
     GoogleMap gogole;
 
@@ -53,8 +64,8 @@ public class Localisation extends Fragment implements LocationListener, OnMapRea
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        latitudeField = (TextView) Objects.requireNonNull(getActivity()).findViewById(R.id.Latitude);
-        longitudeField = (TextView) getActivity().findViewById(R.id.Longitude);
+        GameAddress = (EditText)getActivity().findViewById(R.id.NewGame);
+        Add = (Button)getActivity().findViewById(R.id.AddButton);
 
 
         // Get the location manager 
@@ -94,18 +105,28 @@ public class Localisation extends Fragment implements LocationListener, OnMapRea
     }
 
 
+
+    //Necessaire pour pouvoir implementer OnMapRedayCallback lorsque le fragment contenant la map a bien ete cree.
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-       /*test = Objects.requireNonNull(getActivity()).findViewById(R.id.fragmaps);
-       test.getMapAsync(this);*/
 
+        //Map = fragment dans fragment donc maps est le fragment enfant du fragment localisation
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragmaps);
         //SupportMapFragment mapFragment2 = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.fragmaps);
         //on s'assure que la map est non null pour pouvoir la récupérer. la map est une tache asynchrone pour pouvoir l'actualiser sans freeze l'activite.
         requireNonNull(mapFragment).getMapAsync(this);
 
-        latitudeField = (TextView) Objects.requireNonNull(getActivity()).findViewById(R.id.Latitude);
-        longitudeField = (TextView) getActivity().findViewById(R.id.Longitude);
+        GameAddress = (EditText)Objects.requireNonNull(getActivity()).findViewById(R.id.NewGame);
+        Add = (Button)getActivity().findViewById(R.id.AddButton);
+
+        //clicklistener sur le bouton
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                direccion = GameAddress.getText().toString();
+                ConvertAdresstoLatLng(direccion);
+            }
+        });
     }
 
 
@@ -142,7 +163,53 @@ public class Localisation extends Fragment implements LocationListener, OnMapRea
 
     @Override
     public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(getActivity(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
 
+    }
+    public void ConvertLatLngtoAddress(LatLng latlng){
+        address=new Geocoder(getActivity(),Locale.getDefault());
+        List<Address> places;
+        double lat = (double) (latlng.latitude);
+        double log = (double) (latlng.longitude);
+        try {
+            places =address.getFromLocation(lat,log,1);
+            String localAddress = places.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = places.get(0).getLocality();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public void ConvertAdresstoLatLng(String stg){
+
+    try {
+            address = new Geocoder(getActivity(),Locale.getDefault());
+            List<Address> places;
+            places = address.getFromLocationName(String.valueOf(stg), 10);
+
+
+            if (places == null)
+            {
+                //on lance une exception
+                //à faire car erreur pour l'instant
+
+            }
+            else {
+                /**
+                 * Get latitude and longitude with address and add marker
+                 */
+                Address location = places.get(0);
+                gogole.addMarker(new MarkerOptions()
+                        .position(new LatLng(location.getLatitude(),location.getLongitude()))
+                        .title("New saved game"));
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -152,8 +219,6 @@ public class Localisation extends Fragment implements LocationListener, OnMapRea
             System.out.println("Provider " + provider + " has been selected.");
             onLocationChanged(location);
         } else {
-            latitudeField.setText("Location not available");
-            longitudeField.setText("Location not available");
         }
 
         if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getActivity()), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -176,16 +241,9 @@ public class Localisation extends Fragment implements LocationListener, OnMapRea
             public void onMapClick(LatLng latLng) {
                 gogole.clear();
                 gogole.addMarker(new MarkerOptions().position(latLng));
-
-                double lat = (double) (latLng.latitude);
-                double log = (double) (latLng.longitude);
-
-                latitudeField.setText(String.valueOf(lat));
-                longitudeField.setText(String.valueOf(log));
-
             }
         });
 
-
     }
+
 }
